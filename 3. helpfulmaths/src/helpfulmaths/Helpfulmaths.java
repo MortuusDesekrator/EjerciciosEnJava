c6f4ace72e7b30247e09b45d2609b69b34811888
Walter Alejandro <43525693+MortuusDesekrator@users.noreply.github.com>
2020-02-08 23:55:55 -0400

    Excercise made with math logic

---
+++ 3. helpfulmaths/src/helpfulmaths/Helpfulmaths.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpfulmaths;
+import java.util.*;
+public class Helpfulmaths {
+
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+ 
+        List<String> list = Arrays.asList(s.split("\\+"));
+        list.sort(Comparator.naturalOrder());
+        for (int i=0; i<list.size(); i++)
+        {
+            System.out.print(list.get(i));
+            if (i<list.size()-1) System.out.print("+");
+        }
+        System.out.println();
+    }
+    
+}
